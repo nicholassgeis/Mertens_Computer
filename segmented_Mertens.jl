063a45e0ea994b0742235a49a9eb06a98ac5acc6
nicholassgeis <geis.38@buckeyemail.osu.edu>
2023-11-11 22:51:52 -0500

    more changes

--- segmented_Mertens.jl
+++ segmented_Mertens.jl
@@ -1,3 +1,5 @@
+using Distributions, IterTools, Primes
+
 function arrayloop(A, i, x)
     new_A = Vector{Int8}(undef, x)
     k = 0
@@ -14,23 +16,32 @@ function logfun(n)
     return ceil(Int8, log(4, Int8(n))) | 1
 end
 
-function dict_of_cond_intervals(x)
+function dict_of_cond_intervals()
     num_of_primes = 5
+    len = (2^2)*(3^2)*5*7*11*13
 
-    combi = product([0x00, 0x01], [0x00, 0x01], [0x00, 0x01], [0x00, 0x01], [0x00, 0x01])
+    combi = product([0x00, 0x01], [0x00, 0x01], [0x00, 0x01], [0x00, 0x01], [0x00, 0x01], [0x00, 0x01])
 
-    maindict = Dict{Tuple{UInt8, UInt8}, Vector{UInt8}}()
+    maindict = Dict{NTuple{6, UInt8}, Vector{UInt8}}()
 
     for f in combi
-        m = fill(UInt8(0), x)
+        m = fill(UInt8(0), len)
         for j in 1:num_of_primes
-            for n in prime[j]:prime[j]:x
-                m[n] += 
-                final_array[n] += Int8(1)
+            if f[j] != 0x00
+                for n in prime(j):prime(j):len
+                    m[n] += 0x01
+                end
             end
-            for n in list_of_primes[j]^2:list_of_primes[j]^2:x
+            for n in prime(j)^2:prime(j)^2:len
                 m[n] = m[n] | 0x80
             end
         end
+        maindict[f] = m
     end
+    return maindict
+end
+
+function segmentedrademacher(x0, del, data, condDict)
+    condF = data[1:5]
+    builder = condDict[condF]
 end
